b77c2dd4af427c4fed3471f95df0b08cd7e87826
locke <b.lockejason@gmail.com>
2026-03-22 15:52:08 -0500

    cargo run test

--- macroquad/mqgd_agical/src/main.rs
+++ macroquad/mqgd_agical/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("My First Game")]
+async fn main() {
+    loop {
+        clear_background(RED);
+        draw_text("HELLO MACROQUAD!", 20.0, 20.0, 30.0, DARKGRAY);
+        next_frame().await
+    }
 }
