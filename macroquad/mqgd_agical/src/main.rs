9e1776d23d6ab10cf74018243f50841fffcef1ce
locke <b.lockejason@gmail.com>
2026-03-22 18:51:17 -0500

    collisions of a circle challenge (refs #1)

--- macroquad/mqgd_agical/src/main.rs
+++ macroquad/mqgd_agical/src/main.rs
@@ -2,6 +2,29 @@ use macroquad::{prelude::*, rand::ChooseRandom};
 
 
 struct Shape { size: f32, speed: f32, x: f32, y: f32, color: Color, }
+impl Shape {
+    fn collides_with(&self, other: &Self) -> bool {
+        let rect = other.rect();
+
+        let radius = self.size / 2.0;
+        
+        let close_x = clamp(self.x + radius, rect.x, rect.x + rect.w);
+        let close_y = clamp(self.y + radius, rect.y, rect.y + rect.h);
+
+        let dist_x = self.x + radius - close_x;
+        let dist_y = self.y + radius - close_y;
+
+        (dist_x * dist_x + dist_y * dist_y) < (radius * radius)
+    }
+    fn rect(&self) -> Rect {
+        Rect {
+            x: self.x - self.size / 2.0,
+            y: self.y - self.size / 2.0,
+            w: self.size,
+            h: self.size,
+        }
+    }
+}
 
 #[macroquad::main("My game")]
 
@@ -16,15 +39,21 @@ async fn main() {
         size: 32.0, speed: MOVEMENT_SPEED, x: screen_width()/2.0, y: screen_height()/2.0, color: WHITE
     };
 
+    let mut gameover = false;
     loop {
         clear_background(DARKPURPLE);
         let frame_time = get_frame_time();
-        let velocity = MOVEMENT_SPEED * frame_time;
 
-        if is_key_down(KeyCode::Right) { circle.x += velocity; }
-        if is_key_down(KeyCode::Left) { circle.x -= velocity; }
-        if is_key_down(KeyCode::Down) { circle.y += velocity; }
-        if is_key_down(KeyCode::Up) { circle.y -= velocity; }
+        if !gameover {
+            if is_key_down(KeyCode::Right) { circle.x += MOVEMENT_SPEED * frame_time; }
+            if is_key_down(KeyCode::Left) { circle.x -= MOVEMENT_SPEED * frame_time; }
+            if is_key_down(KeyCode::Down) { circle.y += MOVEMENT_SPEED * frame_time; }
+            if is_key_down(KeyCode::Up) { circle.y -= MOVEMENT_SPEED * frame_time; }
+        }
+
+        if squares.iter().any(|square| circle.collides_with(square)) {
+            gameover = true;
+        }
 
         circle.x = clamp(circle.x, circle.size, screen_width() - circle.size);
         circle.y = clamp(circle.y, circle.size, screen_height() - circle.size);
@@ -37,6 +66,7 @@ async fn main() {
                 y: -size, color: *color.choose().unwrap()
             });
         }
+
         for square in &mut squares { square.y += square.speed * frame_time; }
         squares.retain(|square| square.y < screen_height() + square.size);
 
@@ -44,6 +74,23 @@ async fn main() {
 
         for square in &squares {
             draw_rectangle(square.x - square.size / 2.0, square.y - square.size / 2.0, square.size, square.size, square.color);
+        }
+
+        if gameover && is_key_pressed(KeyCode::Space) {
+            squares.clear();
+            circle.x = screen_width() / 2.0;
+            circle.y = screen_height() / 2.0;
+            gameover = false;
+        }
+
+        if gameover {
+            let text = "GAME OVER!";
+            let text_dimensions = measure_text(text, None, 50, 1.0);
+            draw_text(
+                text, 
+                screen_width() / 2.0 - text_dimensions.width / 2.0, 
+                screen_height() / 2.0, 
+                50.0, RED,);
         }
 
         next_frame().await
