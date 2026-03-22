6d4900feee9c836001e93165d6217dd97656bfa6
locke <b.lockejason@gmail.com>
2026-03-22 16:35:15 -0500

    smooth movement (refs #1)

--- macroquad/mqgd_agical/src/main.rs
+++ macroquad/mqgd_agical/src/main.rs
@@ -5,14 +5,16 @@ async fn main() {
 
     let mut x = screen_width() / 2.0;
     let mut y = screen_height() / 2.0;
+    const MOVEMENT_SPEED: f32 = 200.0;
 
     loop {
         clear_background(DARKPURPLE);
+        let delta_time = get_frame_time();
 
-        if is_key_down(KeyCode::Right) { x += 1.0; }
-        if is_key_down(KeyCode::Left) { x -= 1.0; }
-        if is_key_down(KeyCode::Down) { y += 1.0; }
-        if is_key_down(KeyCode::Up) { y -= 1.0; }
+        if is_key_down(KeyCode::Right) { x += MOVEMENT_SPEED * delta_time; }
+        if is_key_down(KeyCode::Left) { x -= MOVEMENT_SPEED * delta_time; }
+        if is_key_down(KeyCode::Down) { y += MOVEMENT_SPEED * delta_time; }
+        if is_key_down(KeyCode::Up) { y -= MOVEMENT_SPEED * delta_time; }
 
         draw_circle(x, y, 16.0, YELLOW);
 
